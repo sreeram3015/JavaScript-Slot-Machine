48d48861ecc30d019cb2f6138dd804b2f0e0fdfe
Sreeram P <psreeram3015@proton.me>
2024-01-31 20:15:25 +0530

    Implemented getNumberOfLines function
    - Added function to handle user input for the number of lines to bet on
    - Utilized prompt-sync for user input
    - Implemented input validation for non-positive, non-numeric inputs, and values outside the range (1-3)

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")();
 
-const deposit = () => {
+const getDeposit = () => {
     while(true){
         const depositAmount = prompt("Enter your deposit amount: ");
         const integerDepositAmount = parseFloat(depositAmount);
@@ -13,3 +13,20 @@ const deposit = () => {
         }
     }
 }
+
+const getNumberOfLines = () => {
+    while(true){
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid number of lines, try again.")
+        }
+        else{
+            return numberOfLines;
+        }
+    }
+}
+
+const depositAmount = getDeposit();
+const numberOfLines = getNumberOfLines();
