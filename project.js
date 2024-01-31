f7b4c2b6468bd6567884b0fe4dbdc0a5131cf235
Sreeram P <psreeram3015@proton.me>
2024-01-31 22:04:52 +0530

    Implemented getWinnings function for the slot machine
    - Created function to calculate winnings based on the current arrangement of symbols
    - Checked for matching symbols in each row, calculating winnings for matching symbols

--- project.js
+++ project.js
@@ -111,8 +111,30 @@ const display = (rows) => {
     }
 }
 
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+
+    for(let row =0; row<lines; row++){
+        const symbols = rows[row];
+        let allSame = true;
+
+        for(const symbol of symbols){
+            if(symbol != symbols[0]){
+                allSame = false;
+                break;
+            }
+        }
+
+        if(allSame){
+            winnings += bet * SYMBOL_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+}
+
 let balance = getDeposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 const reels = spin();
 display(reels);
+const winnings = getWinnings(rows, bet, numberOfLines)
