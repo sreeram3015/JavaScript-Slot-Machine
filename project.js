b5437407ecb109093a69b87a438ec5e8562a0ca4
Sreeram P <psreeram3015@proton.me>
2024-01-31 20:27:58 +0530

    Defined constants and symbols for the slot machine
    - Set up constant values for the number of rows and columns in the slot machine
    - Defined counts and values for each symbol in SYMBOLS_COUNT and SYMBOL_VALUES objects

--- project.js
+++ project.js
@@ -1,5 +1,23 @@
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
 const getDeposit = () => {
     while(true){
         const depositAmount = prompt("Enter your deposit amount: ");
